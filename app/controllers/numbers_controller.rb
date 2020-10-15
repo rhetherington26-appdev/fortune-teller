f5ce2251257e716905dcfb1bd4168e8808b35488
rhetherington26 <rhetheri@chicagobooth.edu>
2020-10-15 19:50:19 +0000

    1st Commit Debugged Aries, Leo, Sag

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -10,4 +10,17 @@ class NumbersController < ApplicationController
 
     render({ :template => "lottery_stuff/woohoo.html.erb"})
   end 
+
+ def losers
+  @unlucky = Array.new
+
+  5.times do 
+    magma = rand(1...100)
+
+    @unlucky.push(magma)
+  end
+
+  render({ :template => "lottery_stuff/unlucky.html.erb"})
+  end 
+
 end
